b2075b986196aece6d95b33fbc3c855657afd9d8
Denys-Sh <arrowssilvers@gmail.com>
2017-08-29 20:08:26 +0300

    Update loadscript.js

--- js/loadscript.js
+++ js/loadscript.js
@@ -1,21 +1,22 @@
-
   var app = angular.module( 'app', [] )
 .controller('phonebookCtrl', function( $scope ){
+
+  
 
 	
 
   $scope.currentPersonId = -1;
   $scope.phonebook = [
-    { name: 'Cali Roll', phone: '+380-668-622-488', email: 'calli@roll.ua'},
-	{ name: 'Philly bron', phone: '+123-456-789-123', email: 'philli@bron.ia'},
-	{ name: 'Tiger forse', phone: '+365-125-888-589', email: 'tigery@forse.com'},
-	{ name: 'Rainbow darc', phone: '+985-052-5895-489', email: 'rainbow@dark.ea'},
-	{ name: 'Shvarc arnold', phone: '+888-777-888-444', email: 'shvarc@arnold.us'},
-	{ name: 'brus wilis', phone: '+896-568-872-173', email: 'brus@willis.ua'},
-	{ name: 'Chac noris', phone: '+258-238-796-110', email: 'chac@noris.ia'},
-	{ name: 'sivester stalones', phone: '+347-46-206-186', email: 'sivestr@stalones.com'},
-	{ name: 'Ozzy ozbourn', phone: '+314-75-710-189', email: 'Ozzy@ozbourn.ua'},
-	{ name: 'Jackie chan', phone: '+856-987-878-432', email: 'Jackie@chan.us'},
+    { name: 'Cali Roll', phone: '+380-668-622-488', email: 'calli@roll.ua', category:"Леди", date:"2011/01/11"},
+	{ name: 'Philly bron', phone: '+123-456-789-123', email: 'philli@bron.ia', category:"Коллега", date:"1963/11/15"},
+	{ name: 'Tiger forse', phone: '+365-125-888-589', email: 'tigery@forse.com', category:"Леди", date:"1986/12/03"},
+	{ name: 'Rainbow darc', phone: '+985-052-5895-489', email: 'rainbow@dark.ea', category:"Брат", date:"1945/10/02"},
+	{ name: 'Shvarc arnold', phone: '+888-777-888-444', email: 'shvarc@arnold.us', category:"Cum", date:"1951/09/29"},
+	{ name: 'brus wilis', phone: '+896-568-872-173', email: 'brus@willis.ua', category:"Друг", date:"1962/08/18"},
+	{ name: 'Chac noris', phone: '+258-238-796-110', email: 'chac@noris.ia', category:"Друг", date:"1955/06/09"},
+	{ name: 'sivester stalones', phone: '+347-46-206-186', email: 'sivestr@stalones.com', category:"Cum", date:"1956/07/08"},
+	{ name: 'Ozzy ozbourn', phone: '+314-75-710-189', email: 'Ozzy@ozbourn.ua', category:"Коллега", date:"1947/05/12"},
+	{ name: 'Jackie chan', phone: '+856-987-878-432', email: 'Jackie@chan.us', category:"Брат", date:"1963/04/31"},
 
 
   ];
@@ -24,16 +25,26 @@
     $scope.sortReverse  = false;  // обратная сортривка
      $scope.searchName   = ''; 
 
+
+     $scope.showCum = function () {
+               $scope.findName='Cum'
+              
+    };
+
   $scope.addNewPerson = function() {
     if ( $scope.name != '' ) {
       $scope.phonebook.push({
         name: $scope.name,
         phone: $scope.phone,
-        email: $scope.email
+        email: $scope.email,
+        category:$scope.category,
+        date:$scope.date,
       });
       $scope.name = '';
       $scope.phone = '';
-      $scope.email = '';
+     $scope.email = '';
+      $scope.category='';
+      $scope.date='';
     }
   }
   $scope.savePerson = function() {
@@ -42,9 +53,15 @@
       $scope.phonebook[id].name = $scope.name;
       $scope.phonebook[id].phone = $scope.phone;
       $scope.phonebook[id].email = $scope.email;
+      $scope.phonebook[id].category = $scope.category;
+      $scope.phonebook[id].date = $scope.date;
+
+
       $scope.name = '';
       $scope.phone = '';
       $scope.email = '';
+      $scope.category = '';
+      $scope.date = '';
       $scope.currentPersonId = -1;
     }
   }
@@ -53,6 +70,8 @@
     $scope.name = $scope.phonebook[id].name;
     $scope.phone = $scope.phonebook[id].phone;
     $scope.email = $scope.phonebook[id].email;
+    $scope.category = $scope.phonebook[id].category;
+    $scope.date = $scope.phonebook[id].date;
   }
   $scope.deletePerson = function( id ) {
     $scope.phonebook.splice( id, 1 );
@@ -63,5 +82,10 @@ $scope.delete = function(person) {
     var index = $scope.person.indexOf(person); 
     $scope.person.splice(index, 1); 
 };
+
+$scope.takeDay = function (day) {
+        var strDay = new Date(day);
+        return strDay;
+    };
              
 })
